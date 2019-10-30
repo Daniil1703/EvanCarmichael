6cf89ee519cbdfe26e32a3c021dbb21de501e246
daniil <hlistunovd@yandex.ru>
2019-10-30 20:08:04 +0300

    Global changes!

--- polls/views.py
+++ polls/views.py
@@ -14,6 +14,7 @@ def index(request):
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
         return render(request, 'polls/index.html',  {'posts': posts})
 
+
 def login_request(request):
         if request.method == 'POST':
                 form = AuthenticationForm(request=request, data=request.POST)
@@ -23,27 +24,30 @@ def login_request(request):
                         user = authenticate(username=username, password=password)
                         if user is not None:
                                 login(request, user)
-                                messages.info(request, f"You are now logged in as {username}")
+                                messages.info(request, f"Вы авторизовались как {username}")
                                 return redirect('/')
                         else:
-                                messages.error(request, "Invalid username or password.")
+                                messages.error(request, f"NE")
                 else:
-                        messages.error(request, "Invalid username or password.")
+                        messages.success(request, "К сожалению, вы ввели неверный пароль или логин. Проверьте данные еще раз.")
+
         form = AuthenticationForm()
         return render(request = request,template_name = "polls/login.html",context={"form":form})
+
+
 def register(request):
         if request.method == "POST":
                 form = UserCreationForm(request.POST)
                 if form.is_valid():
                         user = form.save()
                         username = form.cleaned_data.get('username')
-                        user.save()
-                        messages.success(request, f'Создан новый аккаунт: {username} ')
+                        messages.success(request, f"Создан новый аккаунт: {username}")
                         login(request, user)
+                        
                         return redirect('polls:index')
                 else:
                         for msg in form.error_messages:
-                                messages.error(request, f'{msg}: {form.error_messages[msg]}')
+                                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
                         return render(request = request,template_name = "polls/register.html",context={"form":form})
         form = UserCreationForm
@@ -51,5 +55,5 @@ def register(request):
 
 def logout_request(request):
         logout(request)
-        messages.info(request, "Logged out successfully!")
+        messages.info(request, f"Вы вышли из системы!")
         return redirect("polls:index")
