69c4e6aecb6a1e38d2c747ae59f1b05e74444f03
Daniil1703 <hlistunovd@yandex.ru>
2019-11-25 00:01:02 +0300

    max posts on the page

--- polls/views.py
+++ polls/views.py
@@ -16,34 +16,34 @@ from .utils import ObjectDetailMixin
 class PostDetail(ObjectDetailMixin, View):
         model = Post
         template = 'polls/post_detail.html'
-   
+
 class TagDetail(ObjectDetailMixin, View):
         model = Tag
         template = 'polls/tag_detail.html'
-        
+
 
 
 # Отображение постов
 def index(request):
         posts = Post.objects.all()
-        paginator = Paginator(posts, 6)
+        paginator = Paginator(posts, 9)
         page_number = request.GET.get('page', 1)
         page = paginator.get_page(page_number)
-        
+
         is_paginated = page.has_other_pages()
-        
+
         if page.has_next():
                 next_url = '?page={}'.format(page.next_page_number())
         else:
                 next_url = ''
-                
+
         context = {
                 'page_object': page,
                 'is_paginated': is_paginated,
                 'next_url': next_url
         }
-        
-        
+
+
         return render(request, 'polls/index.html', context=context)
 
 
@@ -79,7 +79,7 @@ def register(request):
                         username = form.cleaned_data.get('username')
                         messages.success(request, f"Создан новый аккаунт: {username}")
                         login(request, user)
-                        
+
                         return redirect('polls:index')
                 else:
                         for msg in form.error_messages:
