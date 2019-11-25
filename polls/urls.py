8784068c59695e1a6efd45aa80b4b9e15b3c2254
Daniil1703 <hlistunovd@yandex.ru>
2019-11-25 11:25:09 +0300

    Was deleted all trash in the files

--- polls/urls.py
+++ polls/urls.py
@@ -7,10 +7,6 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("login/", views.login_request, name="login"),
-    path('register/', views.register, name="register"),
-    path("logout/", views.logout_request, name="logout"),
-    
     path('post/<str:slug>/', views.PostDetail.as_view(), name="post_detail_url"),
     path('tags/', views.tags_list, name='tags_list'),
     path('tag/<str:slug>/', views.TagDetail.as_view(), name='tag_detail')
