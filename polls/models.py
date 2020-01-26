b70c64576fe4607e9b6497ebff0cb749cb6ebd81
Daniil1703 <hlistunovd@yandex.ru>
2020-01-26 14:56:48 +0300

    Адаптация системы постов и изменение логотипа

--- polls/models.py
+++ polls/models.py
@@ -14,11 +14,11 @@ class Post(models.Model):
 
     date_pub = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200, db_index=True)
+    title_detail = models.CharField(max_length=300, db_index=True)
     slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = RichTextField(blank=True, db_index=True)
     article_image = models.FileField(blank = True,null = True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-    title_detail = RichTextField(blank=True, db_index=True)
 
     class Meta:
         ordering = ('-date_pub',)
