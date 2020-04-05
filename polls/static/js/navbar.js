226ef4ecb8c98ce3facc3e79e04ae9509e430df2
Daniil1703 <hlistunovd@yandex.ru>
2020-04-05 12:28:39 +0300

    Исправление и доработка в навигационном баре

--- polls/static/js/navbar.js
+++ polls/static/js/navbar.js
@@ -1,18 +1,43 @@
+// СКРИПТ ДЛЯ ВЫПАДАЮЩЕГО мЕнЮ ПУНКТА "ЕЩЕ"
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
     }
   }
-} 
+}
+
+// СКРИПТ ДЛЯ АДАПТИВНОГО ПУНКТА МЕНЮ
+function myFunctionToggle(x) {
+    x.classList.toggle("change");
+
+    var y = document.getElementById("myLinks");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+// СКРИПТ ДЛЯ ОТОБРАЖЕНИЕ НАВБАРА ПРИ СКРОЛЛИНГЕ
+var scrollTop = $(window).scrollTop();
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    }
+    else {
+        document.getElementById("navbar").style.top = "-70px";
+    }
+    prevScrollpos = currentScrollPos;
+    if (scrollTop == currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    }
+}
