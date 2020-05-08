11eeaf632ef5c116a774bb4ba1096fe0c6ce0e34
Daniil1703 <hlistunovd@yandex.ru>
2020-05-08 15:46:47 +0300

    When new user was created send signal for create profile

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Profile
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -33,3 +33,4 @@ class CustomUserAdmin(UserAdmin):
     ordering = ('id',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile)
