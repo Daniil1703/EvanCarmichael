580f01f1099e0915114593057e7b82acb130408f
Daniil1703 <hlistunovd@yandex.ru>
2020-05-09 11:59:53 +0300

    method save for profile

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from PIL import Image
 from .managers import CustomUserManager
 
 
@@ -38,3 +39,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Аккаунт {self.user.login_user}'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.picture.path)
+
+        if img.height > 400 or img.width > 400:
+            output_size = (400, 400)
+            img.thumbnail(output_size)
+            img.save(self.picture.path)
